8eb283738bb43fe0c889ef95dd1badb4f6ecce31
Harry Barnes <145344818+harrydbarnes@users.noreply.github.com>
2026-04-20 23:59:39 +0100

    Update version code and name in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.nuvio.tv.harrybarnes.test"
         minSdk = 24
         targetSdk = 36
-        versionCode = 54
-        versionName = "0.6.3-beta-harrybarnes-test"
+        versionCode = 55
+        versionName = "0.6.4-beta-harrybarnes-test"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
