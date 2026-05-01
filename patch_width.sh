2450eb45bbdf97ad9d8988f325894fd47066352f
harrydbarnes <145344818+harrydbarnes@users.noreply.github.com>
2026-05-01 22:08:52 +0000

    Fix sidebar icons shifting during open/close animations
    Adjusted `sidebarDeflateOffsetX` to use a constant offset in both expanded and collapsed states, preventing the horizontal jump of items when the modern sidebar animates. Also reduced `openSidebarWidth` from 262dp to 224dp, adjusting the expansion so focus overlays no longer clip off the left edge of the screen.

--- patch_width.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's/val openSidebarWidth = 262.dp/val openSidebarWidth = 250.dp/g' app/src/main/java/com/nuvio/tv/MainActivity.kt
--- patch_width2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's/val openSidebarWidth = 250.dp/val openSidebarWidth = 224.dp/g' app/src/main/java/com/nuvio/tv/MainActivity.kt
